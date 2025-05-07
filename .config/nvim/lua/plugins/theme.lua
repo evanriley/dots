5272618f75a2770fb6e1f69c1474282bf3d52f8f
Evan Riley <evan@evanriley.net>
2025-05-07 17:56:52 -0400

    nvim: remove auto-theme change based on system

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -16,41 +16,8 @@ return {
         },
       }
     end,
-  },
-  {
-    'RRethy/base16-nvim',
-    opt = {},
-  },
-  {
-    'f-person/auto-dark-mode.nvim',
-    lazy = false,
-    config = function()
-      require('auto-dark-mode').setup {
-        update_interval = 1000,
-        set_dark_mode = function()
-          vim.cmd 'colorscheme kanagawa-paper-ink'
-        end,
-        set_light_mode = function()
-          require('base16-colorscheme').setup {
-            base00 = '#FCFBF9', -- Default Background
-            base01 = '#F7F3EE', -- Lighter Background (status bars)
-            base02 = '#E4DAE0', -- Selection Background
-            base03 = '#BEBBB6', -- Comments, Invisibles, Line Highlighting
-            base04 = '#E5DED5', -- Dark Foreground (status bars)
-            base05 = '#605A52', -- Default Foreground, Caret, Delimiters, Operators
-            base06 = '#7F7A73', -- Light Foreground (not often used)
-            base07 = '#FCFBF9', -- Light Background (not often used)
-            base08 = '#8F5652', -- Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
-            base09 = '#886A44', -- Integers, Boolean, Constants, XML Attributes, Markup Link Url
-            base0A = '#E6DAD8', -- Classes, Markup Bold, Search Text Background
-            base0B = '#747B4D', -- Strings, Inherited Class, Markup Code, Diff Inserted
-            base0C = '#477A7B', -- Support, Regular Expressions, Escape Characters, Markup Quotes
-            base0D = '#556995', -- Functions, Methods, Attribute IDs, Headings
-            base0E = '#83577D', -- Keywords, Storage, Selector, Markup Italic, Diff Changed
-            base0F = '#D8E1E0', -- Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
-          }
-        end,
-      }
+    init = function()
+      vim.cmd 'colorscheme kanagawa-paper-ink'
     end,
   },
 }
